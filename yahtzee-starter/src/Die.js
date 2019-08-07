c061b347f6cba6ef962bc37a962a1dc260862561
Guillermo Elgorriaga <guillermo.elgorriaga@arkusinc.com>
2019-08-07 12:48:20 -0300

    yhatzee

--- yahtzee-starter/src/Die.js
+++ yahtzee-starter/src/Die.js
@@ -2,15 +2,29 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps ={
+    numberWords : ['one', 'two', 'three', 'four', 'five', 'six'],
+    val: Math.floor(Math.random() * 6)
+  };
+  
+  constructor (props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(evt){
+    return this.props.handleClick(this.props.idx);
+  } 
+
+
   render() {
+    const {numberWords, locked, val, disabled, rolling} = this.props;
+    let classes =`Die fas fa-dice-${numberWords[val -1]} fa-5x `;
+    if (locked) classes += "Die-locked";
+    if(rolling) classes+="Die-rolling";
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
+      <i className={classes} onClick={this.handleClick} disabled={disabled} />
     );
   }
 }
