b99d623427d15675d2b527b1c9d3ca729054719f
Guillermo Elgorriaga <guillermo.elgorriaga@arkusinc.com>
2019-09-25 16:19:54 -0300

    asd

--- oktana-weather/src/components/DisplayData.js
+++ oktana-weather/src/components/DisplayData.js
@@ -7,8 +7,8 @@ const DisplayData = (props) => (
     {(props.city && props.ok) &&<div className="DisplayData__info">
      <p className = "DisplayData__key">City: <span className = "DisplayData__value">{props.city}</span></p>
      <p className = "DisplayData__key">Country: <span className = "DisplayData__value">{props.country}</span></p>
-     <p className = "DisplayData__key">Sunrise: <span className = "DisplayData__value">{props.sunrise}</span></p>
-     <p className = "DisplayData__key">Sunset: <span className = "DisplayData__value">{props.sunset}</span></p>
+     <p className = "DisplayData__key">Sunrise: <span className = "DisplayData__value">{Date(props.sunrise)}</span></p>
+     <p className = "DisplayData__key">Sunset: <span className = "DisplayData__value">{Date(props.sunset)}</span></p>
     </div>
     
 }
